3b7d2265d21adbc34d6651ac4222de31c5243d57
jetaehyun <jetaehyun0@gmail.com>
2021-04-19 14:33:23 -0400

    fixed namespace error and byte packing in pack_bits()

--- src/data_packet_light.cpp
+++ src/data_packet_light.cpp
@@ -1,1 +1,27 @@
 #include "../include/data_packet_light.h"
+
+void Data_Packet_Light::create_packet() {
+
+    Data_Manipulation::pack_bits(packet, light, 4);
+    Data_Manipulation::pack_bits(packet + 4, this->user_id, 4);
+
+    for(int i = 0; i < 10; i++) {
+        Data_Manipulation::pack_bits(packet + 8 + i, this->light_ids.at(i), 1);
+
+        Data_Manipulation::pack_bits(packet + 18 + i, this->light_status.at(i), 1);
+    }
+
+    Data_Manipulation::pack_bits(packet + 28, this->disable_all_lights, 1);
+    Data_Manipulation::pack_bits(packet + 36, (uint32_t)100, 4);
+    
+}
+
+
+void Data_Packet_Light::unpack_packet(uint8_t *packet) {
+    
+}
+
+
+uint8_t *Data_Packet_Light::get_packet() {
+    return this->packet;
+}
--- src/sw_doge.cpp
+++ src/sw_doge.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+#include <cstdint>
+
 #include "../include/data_packet_light.h"
+#include "../include/data_manipulation.h"
 
 using std::cout;
 using std::endl;
+using namespace Data_Manipulation;
 
 int main(int argc, const char *argv[]) {
 
-    cout << "TEST...\n" << endl;
 
     return 0;
 }
